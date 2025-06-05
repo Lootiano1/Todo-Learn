35be2ba0cab3cfd4921c9f0be9d4ace0694b98f1
Lootiano1 <lucianograbin12@gmail.com>
2025-06-05 05:05:18 -0300

    localstorage implementation

---
+++ index.js
@@ -0,0 +1,38 @@
+let itens = []
+// localStorage.setItem() ? '' : []
+
+function getItem() {
+    let getItem = document.getElementById("input-add-item").value
+    itens.push(getItem)
+    addItemToList(getItem)
+}
+
+function addItemToList(item) {
+    let lista = document.getElementById("lista")
+    let listItem = document.createElement("div")
+    listItem.id = "item"
+    if (item != "") {
+        for (let i = 0; i < 99; i++) {
+            listItem.innerHTML = `
+                <p>${item}</p>
+                <button id="edit-item" data-index="${i}">Edit</button>
+                <button id="exclude-item" data-index="${i}">Exclude</button>
+            `
+        }
+        lista.appendChild(listItem)
+        registerTasks(item)
+    } else {
+        alert("Não há nada para adiconar") 
+    }
+    
+
+    
+    
+}
+
+function registerTasks(task) {
+    localStorage.getItem("tasks") ?? []
+    task == '' ? console.log("Não há nada para adicionar") : localStorage.setItem("tasks", JSON.stringify(task))
+}
+
+document.getElementById('btn-add-item').addEventListener("click", () => getItem())
